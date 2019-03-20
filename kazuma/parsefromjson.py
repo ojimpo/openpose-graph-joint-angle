790bfaa38b508a410c855ab536db6ac51882dfef
ojimpo <kou997@googlemail.com>
2019-03-20 14:45:56 +0900

    Copy python file to kazuma

---
+++ kazuma/parsefromjson.py
@@ -0,0 +1,45 @@
+import json
+import numpy as np
+from numpy import linalg as LA
+import matplotlib.pyplot as plt
+
+for json_file in ['kazuma_{:012}_keypoints.json'.format(i) for i in range(38)]:
+    l_knee_joint_angle = []
+    with open(json_file) as f:
+        json_object = json.load(json_file)
+
+        l_hip_x = json_object['people'][0]['pose_keypoints_2d'][36]
+        l_hip_y = json_object['people'][0]['pose_keypoints_2d'][37]
+
+        l_knee_x = json_object['people'][0]['pose_keypoints_2d'][39]
+        l_knee_y = json_object['people'][0]['pose_keypoints_2d'][40]
+
+        l_ankle_x = json_object['people'][0]['pose_keypoints_2d'][42]
+        l_ankle_y = json_object['people'][0]['pose_keypoints_2d'][43]
+
+        u = np.array([l_hip_x - l_knee_x, l_hip_y - l_knee_y])
+        v = np.array([l_ankle_x - l_knee_x, l_ankle_y - l_knee_y])
+
+        i = np.inner(u, v)
+        n = LA.norm(u) * LA.norm(v)
+
+        c = i / n
+        a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
+
+        l_knee_joint_angle.append(a)
+
+json_file = open('sample.json', 'r')
+json_object = json.load(json_file)
+
+l_hip_x = json_object['people'][0]['pose_keypoints_2d'][36]
+l_hip_y = json_object['people'][0]['pose_keypoints_2d'][37]
+
+l_knee_x = json_object['people'][0]['pose_keypoints_2d'][39]
+l_knee_y = json_object['people'][0]['pose_keypoints_2d'][40]
+
+l_ankle_x = json_object['people'][0]['pose_keypoints_2d'][42]
+l_ankle_y = json_object['people'][0]['pose_keypoints_2d'][43]
+
+print(str(l_hip_x))
+print(str(l_knee_x))
+print(str(l_ankle_x))
