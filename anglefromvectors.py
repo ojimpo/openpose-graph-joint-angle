e2604b63a567de30c3dcb8489496a715d12d757a
ojimpo <kou997@googlemail.com>
2019-03-20 13:56:56 +0900

    Import sample code

--- anglefromvectors.py
+++ anglefromvectors.py
@@ -1,5 +1,15 @@
-import numpy as numpy
+import numpy as np
+from numpy import linalg as LA
 
-def angle(x, y):
-    dot_xy = np.dot(x,y)
-    norm_x = np.lin
+u = np.array([3, 4])
+v = np.array([-4, 3])
+
+i = np.inner(u, v)
+n = LA.norm(u) * LA.norm(v)
+
+c = i / n
+a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
+
+print(a)
+
+# 90.0
