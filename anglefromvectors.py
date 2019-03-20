428cb1701cd11ab16f49099724d8af76223917ff
ojimpo <kou997@googlemail.com>
2019-03-20 14:11:31 +0900

    Use actual value

--- anglefromvectors.py
+++ anglefromvectors.py
@@ -1,6 +1,15 @@
 import numpy as np
 from numpy import linalg as LA
 
+l_hip_x = 90.656
+l_hip_y = 273.001
+
+l_knee_x = 134.778
+l_knee_y = 337.153
+
+l_ankle_x = 198.88
+l_ankle_y = 387.174
+
 u = np.array([3, 4])
 v = np.array([-4, 3])
 
