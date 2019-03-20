22e442e178b052a7bd5d786c5fa1f067fa81506b
ojimpo <kou997@googlemail.com>
2019-03-20 18:02:40 +0900

    Not to use .get

--- kazuma/parsefromjson.py
+++ kazuma/parsefromjson.py
@@ -8,14 +8,14 @@ l_knee_joint_angle = []
 with open('kazuma_000000000034_keypoints.json') as f:
         json_object = json.load(f)
 
-        l_hip_x = json_object.get('["people"][0]["pose_keypoints_2d"][36]', 1)
-        l_hip_y = json_object.get('["people"][0]["pose_keypoints_2d"][37]', 1)
+        l_hip_x = json_object.get["people"][0]["pose_keypoints_2d"][36]
+        l_hip_y = json_object.get["people"][0]["pose_keypoints_2d"][37]
 
-        l_knee_x = json_object.get('["people"][0]["pose_keypoints_2d"][39]', 1)
-        l_knee_y = json_object.get('["people"][0]["pose_keypoints_2d"][40]', 1)
+        l_knee_x = json_object.get["people"][0]["pose_keypoints_2d"][39]
+        l_knee_y = json_object.get["people"][0]["pose_keypoints_2d"][40]
 
-        l_ankle_x = json_object.get('["people"][0]["pose_keypoints_2d"][42]', 1)
-        l_ankle_y = json_object.get('["people"][0]["pose_keypoints_2d"][43]', 1)
+        l_ankle_x = json_object.get["people"][0]["pose_keypoints_2d"][42]
+        l_ankle_y = json_object.get["people"][0]["pose_keypoints_2d"][43]
 
         u = np.array([l_hip_x - l_knee_x, l_hip_y - l_knee_y])
         v = np.array([l_ankle_x - l_knee_x, l_ankle_y - l_knee_y])
