e8bd36bc8fa7a30d32f48c627811648da0db0ad1
ojimpo <kou997@googlemail.com>
2019-03-20 16:06:24 +0900

    Use json.load function, delete and copy files

--- kazuma/parsefromjson_notusefor.py
+++ kazuma/parsefromjson_notusefor.py
@@ -7,15 +7,14 @@ l_knee_joint_angle = []
 
 for json_file in ['kazuma_{:012d}_keypoints.json'.format(i) for i in range(38)]:
     with open(json_file) as f:
-        json_object = json.load(f)
-        l_hip_x = json_object["people"][0]["pose_keypoints_2d"][36]
-        l_hip_y = json_object["people"][0]["pose_keypoints_2d"][37]
+        l_hip_x = f["people"][0]["pose_keypoints_2d"][36]
+        l_hip_y = f["people"][0]["pose_keypoints_2d"][37]
 
-        l_knee_x = json_object["people"][0]["pose_keypoints_2d"][39]
-        l_knee_y = json_object["people"][0]["pose_keypoints_2d"][40]
+        l_knee_x = f["people"][0]["pose_keypoints_2d"][39]
+        l_knee_y = f["people"][0]["pose_keypoints_2d"][40]
 
-        l_ankle_x = json_object["people"][0]["pose_keypoints_2d"][42]
-        l_ankle_y = json_object["people"][0]["pose_keypoints_2d"][43]
+        l_ankle_x = f["people"][0]["pose_keypoints_2d"][42]
+        l_ankle_y = f["people"][0]["pose_keypoints_2d"][43]
 
         u = np.array([l_hip_x - l_knee_x, l_hip_y - l_knee_y])
         v = np.array([l_ankle_x - l_knee_x, l_ankle_y - l_knee_y])
