6847fd0dd4b0f3685b1d125e44227f9308cb9f2b
ojimpo <kou997@googlemail.com>
2019-03-21 10:55:53 +0900

    Fix formula for the angle

--- kazuma/parsefromjson.py
+++ kazuma/parsefromjson.py
@@ -22,6 +22,12 @@ for json_file in ['kazuma_{:012d}_keypoints.json'.format(i) for i in range(38)]:
                 l_ankle_x = json_object['people'][0]['pose_keypoints_2d'][42]
                 l_ankle_y = json_object['people'][0]['pose_keypoints_2d'][43]
 
+                u = np.array([l_hip_x - l_knee_x, l_hip_y - l_knee_y])
+                v = np.array([l_ankle_x - l_knee_x, l_ankle_y - l_knee_y])
+
+                i = np.inner(u, v)
+                n = LA.norm(u) * LA.norm(v)
+
                 c = i / n
                 a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
 
