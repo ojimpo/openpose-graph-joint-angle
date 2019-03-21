c5af5f63c222d287147afc12b6c507ce4ca73e47
ojimpo <kou997@googlemail.com>
2019-03-21 10:53:28 +0900

    To check length of list

--- kazuma/parsefromjson.py
+++ kazuma/parsefromjson.py
@@ -8,27 +8,22 @@ l_knee_joint_angle = []
 for json_file in ['kazuma_{:012d}_keypoints.json'.format(i) for i in range(38)]:
     with open(json_file) as f:
         json_object = json.load(f)
+        json_object_length = len(json_object['people'])
 
-        l_hip_x = json_object.get('["people"][0]["pose_keypoints_2d"][36]', 0)
-        l_hip_y = json_object.get('["people"][0]["pose_keypoints_2d"][37]', 0)
+        if json_object_length == 0:
+                a = 0
+        else:
+                l_hip_x = json_object['people'][0]['pose_keypoints_2d'][36]
+                l_hip_y = json_object['people'][0]['pose_keypoints_2d'][37]
 
-        l_knee_x = json_object.get('["people"][0]["pose_keypoints_2d"][39]', 0)
-        l_knee_y = json_object.get('["people"][0]["pose_keypoints_2d"][40]', 0)
+                l_knee_x = json_object['people'][0]['pose_keypoints_2d'][39]
+                l_knee_y = json_object['people'][0]['pose_keypoints_2d'][40]
 
-        l_ankle_x = json_object.get('["people"][0]["pose_keypoints_2d"][42]', 0)
-        l_ankle_y = json_object.get('["people"][0]["pose_keypoints_2d"][43]', 0)
+                l_ankle_x = json_object['people'][0]['pose_keypoints_2d'][42]
+                l_ankle_y = json_object['people'][0]['pose_keypoints_2d'][43]
 
-        u = np.array([l_hip_x - l_knee_x, l_hip_y - l_knee_y])
-        v = np.array([l_ankle_x - l_knee_x, l_ankle_y - l_knee_y])
-
-        i = np.inner(u, v)
-        n = LA.norm(u) * LA.norm(v)
-
-        if n != 0:
                 c = i / n
                 a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
-        else:
-                a = 0
 
         l_knee_joint_angle.append(a)
 
