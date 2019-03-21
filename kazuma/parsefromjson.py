ce8712374ffa7ff2bb6b1d748a8ae011e608bd4a
ojimpo <kou997@googlemail.com>
2019-03-21 11:00:17 +0900

    Conditional branch when divide by 0

--- kazuma/parsefromjson.py
+++ kazuma/parsefromjson.py
@@ -28,8 +28,11 @@ for json_file in ['kazuma_{:012d}_keypoints.json'.format(i) for i in range(38)]:
                 i = np.inner(u, v)
                 n = LA.norm(u) * LA.norm(v)
 
-                c = i / n
-                a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
+                if n == 0:
+                        a = 0
+                else:
+                        c = i / n
+                        a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
 
         l_knee_joint_angle.append(a)
 
