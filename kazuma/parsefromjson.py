7635b6818fe193e875b52ea7aa6eec778e7f4a31
ojimpo <kou997@googlemail.com>
2019-04-13 15:39:55 +0900

    Make to use def to calculate joint angles

--- kazuma/parsefromjson.py
+++ kazuma/parsefromjson.py
@@ -12,29 +12,29 @@ def point_x(number):
 def point_y(number):
         return (number * 3) + 1
 
-# def joint_angle(point1, point2, point3):
-#         p1_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point1)]
-#         p1_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point1)]
+def joint_angle(point1, point2, point3):
+        p1_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point1)]
+        p1_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point1)]
 
-#         p2_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point2)]
-#         p2_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point2)]
+        p2_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point2)]
+        p2_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point2)]
 
-#         p3_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point3)]
-#         p3_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point3)]
+        p3_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point3)]
+        p3_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point3)]
 
-#         u = np.array([p1_x - p2_x, p1_y - p2_y])
-#         v = np.array([p3_x - p2_x, p3_y - p2_y])
+        u = np.array([p1_x - p2_x, p1_y - p2_y])
+        v = np.array([p3_x - p2_x, p3_y - p2_y])
 
-#         i = np.inner(u, v)
-#         n = LA.norm(u) * LA.norm(v)
+        i = np.inner(u, v)
+        n = LA.norm(u) * LA.norm(v)
 
-#         if n == 0:
-#                 a = 0
-#         else:
-#                 c = i / n
-#                 a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
+        if n == 0:
+                a = 0
+        else:
+                c = i / n
+                a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
 
-#         return a
+        return a
 
 l_knee_joint_angle = []
 
@@ -43,37 +43,39 @@ for json_file in natsorted(glob.glob('*_keypoints.json')):
         json_object = json.load(f)
         json_object_length = len(json_object['people'])
 
+        print(json_object_length)
+
         if json_object_length == 0:
-                a = 0
+                angle = 0
         else:
-                # joint_angle(12, 13, 14)
+                angle = joint_angle(point1 = 12, point2 = 13, point3 = 14)
 
-                point1 = 12
-                point2 = 13
-                point3 = 14
+                # point1 = 12
+                # point2 = 13
+                # point3 = 14
 
-                p1_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point1)]
-                p1_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point1)]
+                # p1_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point1)]
+                # p1_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point1)]
 
-                p2_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point2)]
-                p2_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point2)]
+                # p2_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point2)]
+                # p2_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point2)]
 
-                p3_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point3)]
-                p3_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point3)]
+                # p3_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point3)]
+                # p3_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point3)]
 
-                u = np.array([p1_x - p2_x, p1_y - p2_y])
-                v = np.array([p3_x - p2_x, p3_y - p2_y])
+                # u = np.array([p1_x - p2_x, p1_y - p2_y])
+                # v = np.array([p3_x - p2_x, p3_y - p2_y])
 
-                i = np.inner(u, v)
-                n = LA.norm(u) * LA.norm(v)
+                # i = np.inner(u, v)
+                # n = LA.norm(u) * LA.norm(v)
 
-                if n == 0:
-                        a = 0
-                else:
-                        c = i / n
-                        a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
+                # if n == 0:
+                #         a = 0
+                # else:
+                #         c = i / n
+                #         a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
 
-        l_knee_joint_angle.append(a)
+        l_knee_joint_angle.append(angle)
 
 print(l_knee_joint_angle)
 
