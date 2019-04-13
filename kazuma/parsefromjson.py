8ed7c16919203e7026099dfad70d9d32ab3ce0ad
ojimpo <kou997@googlemail.com>
2019-04-13 12:27:47 +0900

    Test if argument name can set to array name

--- kazuma/parsefromjson.py
+++ kazuma/parsefromjson.py
@@ -14,7 +14,9 @@ def point_y(number):
 
 json_object = json.load(f)
 
-def joint_angle(point1, point2, point3):
+def joint_angle(name, point1,point2, point3):
+        name = []
+
         p1_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point1)]
         p1_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point1)]
 
@@ -38,7 +40,34 @@ def joint_angle(point1, point2, point3):
 
         return a
 
-l_knee_joint_angle = []
+        name.append(a)
+
+
+# def joint_angle(point1, point2, point3):
+#         p1_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point1)]
+#         p1_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point1)]
+
+#         p2_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point2)]
+#         p2_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point2)]
+
+#         p3_x = json_object['people'][0]['pose_keypoints_2d'][point_x(point3)]
+#         p3_y = json_object['people'][0]['pose_keypoints_2d'][point_y(point3)]
+
+#         u = np.array([p1_x - p2_x, p1_y - p2_y])
+#         v = np.array([p3_x - p2_x, p3_y - p2_y])
+
+#         i = np.inner(u, v)
+#         n = LA.norm(u) * LA.norm(v)
+
+#         if n == 0:
+#                 a = 0
+#         else:
+#                 c = i / n
+#                 a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
+
+#         return a
+
+# l_knee_joint_angle = []
 
 for json_file in natsorted(glob.glob('*_keypoints.json')):
     with open(json_file) as f:
@@ -47,7 +76,7 @@ for json_file in natsorted(glob.glob('*_keypoints.json')):
         if json_object_length == 0:
                 a = 0
         else:
-                joint_angle(12, 13, 14)
+                joint_angle(l_knee_joint_angle, 12, 13, 14)
 
                 # point1 = 12
                 # point2 = 13
@@ -74,7 +103,7 @@ for json_file in natsorted(glob.glob('*_keypoints.json')):
                 #         c = i / n
                 #         a = np.rad2deg(np.arccos(np.clip(c, -1.0, 1.0)))
 
-        l_knee_joint_angle.append(a)
+        # l_knee_joint_angle.append(a)
 
 print(l_knee_joint_angle)
 
